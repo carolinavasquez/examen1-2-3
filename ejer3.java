8876d4250d5944de655dabbc839b595514d6592b
carolinavasquez <deysicarolina@unitec.com>
2014-03-20 20:07:12 -0600

    Update ejer3.java

--- ejer3.java
+++ ejer3.java
@@ -5,21 +5,55 @@ public class ejer3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		  Scanner teclado=new Scanner(System.in);
-	        int suma,f,valor,promedio;
-	        suma=0;
-	        for(f=1;f<=20;f++) {
-	            System.out.print("Ingrese valor:");
-	            valor=teclado.nextInt();
-	            suma=suma+valor;
-	        }
-	       
-	        promedio=suma/10;
-	        System.out.print("El promedio es:");
-	        System.out.print(promedio);
-	    }
+		 	System.out.println ("Introduce el 1 numero:");
+		Scanner deysi=new Scanner (System.in);
+		double num1=deysi.nextDouble();
+		System.out.println ("Introduce el 2 numero:");
+		double num2=deysi.nextDouble();
+		System.out.println ("Introduce el 3 numero:");
+		double num3=deysi.nextDouble();
+		System.out.println ("Introduce el 4 numero:");
+		double num4=deysi.nextDouble();
+		System.out.println ("Introduce el 5 numero:");
+		double num5=deysi.nextDouble();
+		System.out.println ("Introduce el 6 numero:");
+		double num6=deysi.nextDouble();
+		System.out.println ("Introduce el 7 numero:");
+		double num7=deysi.nextDouble();
+		System.out.println ("Introduce el 8 numero:");
+		double num8=deysi.nextDouble();
+		System.out.println ("Introduce el 9 numero:");
+		double num9=deysi.nextDouble();
+		System.out.println ("Introduce 10 numero:");
+		double num10=deysi.nextDouble();
+		System.out.println ("Introduce el 11 numero:");
+		double num11=deysi.nextDouble();
+		System.out.println ("Introduce el 12 numero:");
+		double num12=deysi.nextDouble();
+		System.out.println ("Introduce el 13 numero:");
+		double num13=deysi.nextDouble();
+		System.out.println ("Introduce el 14 numero:");
+		double num14=deysi.nextDouble();
+		System.out.println ("Introduce el 15 numero:");
+		double num15=deysi.nextDouble();
+		System.out.println ("Introduce el 16 numero:");
+		double num16=deysi.nextDouble();
+		System.out.println ("Introduce el 17 numero:");
+		double num17=deysi.nextDouble();
+		System.out.println ("Introduce el 18 numero:");
+		double num18=deysi.nextDouble();
+		System.out.println ("Introduce el 19 numero:");
+		double num19=deysi.nextDouble();
+		System.out.println ("Introduce el 20 numero:");
+		double num20=deysi.nextDouble();
+
+		
+		double promedio = ((num1+num2+num3
+		+num4+num5+num6+num7+num8+num9+num10+num11+num12+num13+num14+num15+num16+num17+num18+num19+num20)/10);
+        System.out.print("el total de promedio de todos los numeros es:");
+        System.out.print(promedio);
+		}
 
 	}
-	
 
 
